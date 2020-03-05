b4a0a7ce64e67b2a24a5fd0d40fc5b9d04d47f77
rocket7642 <49769828+rocket7642@users.noreply.github.com>
2020-03-05 13:56:43 -0800

    Basic Index Code added
    Collected all the words, now to move them to the main index

--- src/MyMiniSearchEngine.java
+++ src/MyMiniSearchEngine.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.*;
 
 public class MyMiniSearchEngine {
     // default solution. OK to change.
@@ -18,7 +19,27 @@ public class MyMiniSearchEngine {
     // each item in the List is considered a document.
     // assume documents only contain alphabetical words separated by white spaces.
     private void index(List<String> texts) {
-        //homework
+
+        for(int f = 0; f < texts.size(); f++){
+            Map<String, List<Integer>> catagories = new HashMap<String, List<Integer>>();
+            String temp[] = texts.get(f).split(" ");
+
+            List<List<Integer>> input = new ArrayList<List<Integer>>();
+            for(int i = 0; i < temp.length; i++){
+                List<Integer> holder = new ArrayList<Integer>();
+                if(!(catagories.containsKey(temp[i]))){
+                    catagories.put(temp[i], holder);
+                }
+                if(catagories.containsKey(temp[i])){
+                    catagories.get(temp[i]).add(i);
+                }
+            }
+
+            for(int i = 0; i < temp.length; i++){
+                System.out.println(catagories.get(temp[i]) + ":" + temp[i]);
+            }
+            System.out.println("---");
+        }
     }
 
     // search(key) return all the document ids where the given key phrase appears.
