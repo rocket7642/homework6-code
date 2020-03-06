c68ea9cbd5f8ae26f1d650eaa9f38bc373ae349e
rocket7642 <49769828+rocket7642@users.noreply.github.com>
2020-03-05 17:42:47 -0800

    indexed complete
    Need to set the tests to use the data.

--- src/MyMiniSearchEngine.java
+++ src/MyMiniSearchEngine.java
@@ -7,19 +7,22 @@ public class MyMiniSearchEngine {
     // default solution. OK to change.
     // do not change the signature of index()
     private Map<String, List<List<Integer>>> indexes;
+    private Map<String, Map<Integer, List<Integer>>> indexes2;
+    private int counter;
 
     // disable default constructor
     private MyMiniSearchEngine() {
     }
 
     public MyMiniSearchEngine(List<String> documents) {
+        counter = documents.size();
         index(documents);
     }
 
     // each item in the List is considered a document.
     // assume documents only contain alphabetical words separated by white spaces.
     private void index(List<String> texts) {
-
+        indexes2 = new HashMap<String, Map<Integer, List<Integer>>>();
         for(int f = 0; f < texts.size(); f++){
             Map<String, List<Integer>> catagories = new HashMap<String, List<Integer>>();
             String temp[] = texts.get(f).split(" ");
@@ -28,7 +31,10 @@ public class MyMiniSearchEngine {
             for(int i = 0; i < temp.length; i++){
                 List<Integer> holder = new ArrayList<Integer>();
                 if(!(catagories.containsKey(temp[i]))){
-                    catagories.put(temp[i], holder);
+                    catagories.put(temp[i], new ArrayList<Integer>());
+                }
+                if(!(indexes2.containsKey(temp[i]))){
+                    indexes2.put(temp[i], new HashMap<Integer, List<Integer>>());
                 }
                 if(catagories.containsKey(temp[i])){
                     catagories.get(temp[i]).add(i);
@@ -36,9 +42,20 @@ public class MyMiniSearchEngine {
             }
 
             for(int i = 0; i < temp.length; i++){
-                System.out.println(catagories.get(temp[i]) + ":" + temp[i]);
+                if(indexes2.containsKey(temp[i])){
+                    indexes2.get(temp[i]).put(f,catagories.get(temp[i]));
+                }
+                //System.out.println(catagories.get(temp[i]) + ":" + temp[i]);
             }
-            System.out.println("---");
+            //System.out.println("---");
+        }
+        
+        for(Map.Entry entry : indexes2.entrySet()){
+            System.out.print(entry.getKey() + ":");
+            Map<Integer, List<Integer>> temp = (Map)entry.getValue();
+            for(Map.Entry entry2 : temp.entrySet()){
+                System.out.println(entry2.getKey()+ ":" + entry2.getValue());
+            }
         }
     }
 
